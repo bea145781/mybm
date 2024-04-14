a5156fed46229d4a8b744708ec418d8dd59d52b9
xiong.c <xiong@xiongcdeMacBook-Pro.local>
2024-04-14 13:49:08 +0800

    main

--- src/main.js
+++ src/main.js
@@ -1,55 +1,38 @@
 const { Console } = require('console')
+require('dotenv').config()
 const WebsocketStream = require('./websocketStream')
 const logger = new Console({ stdout: process.stdout, stderr: process.stderr })
 const WebsocketAPI = require('./websocketAPI')
+const Spot = require('./spot')
 
+const apiKey = process.env.API_KEY
+const apiSecret = process.env.API_SECRET
+const client = new Spot(apiKey, apiSecret, { baseURL: 'https://api.binance.com' })
+
+const k_time_gap = 5
+const k_time = k_time_gap + 'm'
+
+let isOperated = false
+
+const unit_1_btc_amount = 0.001
+const unit_2_btc_amount = unit_1_btc_amount * 2
 let history_close_prices = []
-let close_prices = []
-let btc_amount = 0
-let usdt_amount = 1000
+
+let borrow_time = 0
+
+let borrow_btc_amount = 0
+let spot_btc_amount = 0
+let spot_usdt_amount = 10000
+
 getHistoryKlines();
 
-//setTimeout(() => console.log(history_close_prices[history_close_prices.length - 1]), 12000)
-
-// setTimeout(() => console.log(calculateRSI(getlist(177),6).slice(-5)), 14000)
-// setTimeout(() => console.log(calculateRSI(getlist(177),12).slice(-5)), 14000)
-// setTimeout(() => console.log(calculateRSI(getlist(177),24).slice(-5)), 14000)
-// setTimeout(() => console.log("r6", calculateRSI2(close_prices,6)), 14000)
-// setTimeout(() => console.log("r12", calculateRSI2(close_prices,12)), 15000)
-// setTimeout(() => console.log("r24", calculateRSI2(close_prices,24)), 16000)
-
 setTimeout(() => run(), 12000)
-//setTimeout(() => console.log(calculateRSI(getlist(30), 6)), 14000)
-
-//console.log(calculateRSI([50,35,47,45,23,41,38], 6))
-// const callbacks = {
-//     open: () => logger.debug('Connected with Websocket server'),
-//     close: () => logger.debug('Disconnected with Websocket server'),
-//     message: data => logger.info(data)
-// }
-  
-// const websocketStreamClient = new WebsocketStream({ logger, callbacks, combinedStreams: true })
-  
-// websocketStreamClient.kline('btcusdt', '1m')
-  
-// //setTimeout(() => websocketStreamClient.unsubscribe('btcusdt@kline_1m'), 6000)
-// setTimeout(() => websocketStreamClient.disconnect(), 12000)
-
-// for(let i = 0, len = klines.length; i < len; i++) {
-//     klines[i] = i+1;
-// }
-// console.log(klines)
-
-// klines.shift()
-// klines.push(25)
-// console.log(klines)
-
 
 function getHistoryKlines() {
     const callbacks = {
         open: (client) => {
             logger.debug('Connected with Websocket server1')
-            client.klines('BTCUSDT', '1m', { limit: 190 })
+            client.klines('BTCFDUSD', k_time, { limit: 185 })
         },
         close: () => {
             logger.debug('Disconnected with Websocket server1')
@@ -74,10 +57,26 @@ function run() {
         open: () => logger.debug('Connected with Websocket server'),
         close: () => logger.debug('Disconnected with Websocket server'),
         message: data => {
+            const now_time = Date.now()
+            
+            // 检查是否拿着现货btc还没有还清btc欠款，且在一小时的最后3分钟内
+            if(borrow_btc_amount > 0){
+                const minute = (now_time - borrow_time) / 60000 % 60
+                if(57 < minute && minute < 59){
+                    if(spot_btc_amount > 0){
+                        if(!isOperated){
+                            console.log("# hand_repay_btc")
+                            isOperated = true
+                            hand_repay_btc(borrow_btc_amount)
+                        }
+                    }
+                }
+            }
+
             const kline = JSON.parse(data).data.k
             const h_last_close_price = history_close_prices[history_close_prices.length - 1]
             if (h_last_close_price[0] < kline.t) {  // add kline
-                if (h_last_close_price[0] + 60000 != kline.t) {
+                if (h_last_close_price[0] + k_time_gap*60000 != kline.t) {
                     throw new Error("miss gap kline");
                 }
                 history_close_prices.shift();
@@ -87,47 +86,123 @@ function run() {
             } else {
                 throw new Error("h_last_close_price[0] > kline.t??");
             }
-
+            
             const last_close_prices = getClosePricelist(180)
             const r6 = calculateRSI2(last_close_prices,6)
             const r12 = calculateRSI2(last_close_prices,12)
             const r24 = calculateRSI2(last_close_prices,24)
-            // console.log("r6:", calculateRSI2(last_close_prices,6))
-            // console.log("r12:", calculateRSI2(last_close_prices,12))
-            // console.log("r24:", calculateRSI2(last_close_prices,24))
+        
             makeOrder(r6, r12, r24, kline.c)
-            console.log("------------------------------")
+            console.log("----------------------------------------------------")
         }
     }
   
     const websocketStreamClient = new WebsocketStream({ logger, callbacks, combinedStreams: true })
-  
-    websocketStreamClient.kline('btcusdt', '1m')
+
+    websocketStreamClient.kline('btcfdusd', k_time)
   
     //setTimeout(() => websocketStreamClient.unsubscribe('btcusdt@kline_1m'), 6000)
     //setTimeout(() => websocketStreamClient.disconnect(), 50000)
 }
 
 function makeOrder(r6, r12, r24, now_price) {
-    if(r6 > r12 && r12 > r24){  // should long
-        if(btc_amount == 0){
-            // buy btc
-            btc_amount += usdt_amount / now_price
-            usdt_amount = 0
+    if(r6 > r12 && r12 > r24){  // 改成多方向
+        console.log("r6 > r12 > r24, long!!")
+        
+        // 已经拥有现货，不进行额外操作
+        if(spot_btc_amount > 0){
+            console.log("现>0 PASS")
         }
-    } else if (r6 < r12 && r12 < r24) {  // should short
-        if(btc_amount > 0){
-            // sell btc
-            usdt_amount += btc_amount * now_price
-            btc_amount = 0
+
+        // 借款0单位 现货0单位 普通模式买1单位
+        else if(spot_btc_amount == 0 && borrow_btc_amount == 0){
+            if(!isOperated){
+                console.log("借0 现0 普通买1")
+                isOperated = true
+                buy_btc(unit_1_btc_amount)
+            }else{
+                console.log("isOperated")
+            }
+        }
+
+        // 借款1单位 现货0单位 根据借款时长选择模式买2单位
+        else if (spot_btc_amount == 0 && borrow_btc_amount == unit_1_btc_amount){
+            if(!isOperated){
+                const minute = (Date.now() - borrow_time) / 60000 % 60
+                // 检查是否在借贷一小时的最后3分钟内，如果是则，采用自动还款的购买btc方式
+                if(57 < minute && minute < 59){ // 自动还款模式
+                    console.log("借1 现0 还款买2")
+                    isOperated = true
+                    repay_buy_btc(unit_2_btc_amount)
+                } else { // 普通模式
+                    console.log("借1 现0 普通买2")
+                    isOperated = true
+                    buy_btc(unit_2_btc_amount)
+                }
+            }else{
+                console.log("isOperated")
+            }
+        }
+
+    } else if (r6 < r12 && r12 < r24) {  // 改成空方向
+        console.log("r6 < r12 < r24, short!!")
+
+        // 借款1单位 现货0单位，不进行额外操作
+        if(borrow_btc_amount == unit_1_btc_amount && spot_btc_amount == 0){
+            console.log("借1 现0 PASS")
+        }
+
+        // 借款0单位 现货0单位，借款模式卖1单位
+        else if( borrow_btc_amount == 0 && spot_btc_amount == 0){
+            if(!isOperated){
+                console.log("借0 现0 借款卖1")
+                isOperated = true
+                borrow_sell_btc(unit_1_btc_amount)
+            }else{
+                console.log("isOperated")
+            }
+        }
+
+        // 借款0单位 现货1单位，借款模式卖2单位
+        else if( borrow_btc_amount == 0 && spot_btc_amount == unit_1_btc_amount){
+            if(!isOperated){
+                isOperated = true
+                console.log("借0 现1 借款卖2")
+                borrow_sell_btc(unit_2_btc_amount)
+            }else{
+                console.log("isOperated")
+            }
+        }
+
+        // 借款1单位 现货2单位，普通模式卖2单位
+        else if( borrow_btc_amount == unit_1_btc_amount && spot_btc_amount == unit_2_btc_amount){
+            if(!isOperated){
+                isOperated = true
+                console.log("借1 现2 普通卖2")
+                sell_btc(unit_2_btc_amount)
+            }else{
+                console.log("isOperated")
+            }
         }
     }
-    console.log("r6",r6)
-    console.log("r12",r12)
-    console.log("r24",r24)
+    console.log("r6", r6.toFixed(2), "| r12", r12.toFixed(2), "| r24", r24.toFixed(2))
+   
+    
+    console.log("borrow_time",borrow_time)
+    let minute = 0
+    if(borrow_btc_amount > 0){
+        minute = (Date.now() - borrow_time) / 60000 % 60
+    }
+    console.log("minute", minute)
+
+    console.log("borrow_btc_amount", borrow_btc_amount)
+
+    console.log("spot_btc_amount", spot_btc_amount)
+
     console.log("price", now_price)
-    console.log("btc_amount", btc_amount)
-    console.log("usdt_amount", usdt_amount)
+    // console.log("spot_usdt_amount", spot_usdt_amount)
+    // const asset_usdt_amount = spot_usdt_amount + (spot_btc_amount * now_price) - (borrow_btc_amount * now_price)
+    // console.log("asset_usdt_amount", asset_usdt_amount)
 }
 
 function getClosePricelist(period) {
@@ -143,26 +218,6 @@ function getClosePricelist(period) {
     //close_list.pop();
     return close_list;
 }
-// function calculateRSI() {
-//     let rise_sum_6 = 0;
-//     let fall_sum_6 = 0;
-//     for(let i = 2; i < 8; i++) {
-//         const length = klines.length;
-//         const chg = klines[length - i][12];
-//         if(chg > 0){
-//             rise_sum_6 += chg
-//             console.log(chg)
-//         }else{
-//             fall_sum_6 += chg
-//             console.log(chg)
-//         }
-//     }
-//     console.log(rise_sum_6 / 6)
-//     console.log(fall_sum_6 / 6)
-//     const rsi6 = (rise_sum_6 / 6) / ((rise_sum_6 / 6) + ((-fall_sum_6) / 6)) * 100
-//     console.log("rsi6:", rsi6)
-// }
-
 
 function calculateRSI(data, period) {
     if (data.length <= period) {
@@ -252,11 +307,129 @@ function calculateRSI2(prices, period = 14) {
   
     return rsi;
   }
-  
 
-// // 使用示例
-// const periods = 14; // RSI的时间周期
-// const values = [132.54, 131.45, 130.23, 130.47, 133.72, 134.27, 134.50, 134.80, 135.00, 135.00, 134.80, 134.80, 134.80, 135.00, 135.00, 135.00, 135.00]; // 股票价格数组
-// console.log("1",calculateRSI(values, periods))
 
-// console.log("3",calculateRSI2(values, periods))
+// 自动借款模式卖
+function borrow_sell_btc(sell_amount) {
+
+    client.newMarginOrder(
+        'BTCFDUSD', // symbol
+        'SELL',
+        'MARKET',
+        {
+          quantity: sell_amount,
+          newOrderRespType: 'RESULT',
+          sideEffectType: 'MARGIN_BUY',
+          autoRepayAtCancel: true
+        }
+      ).then(response => {
+        const result = response.data
+        if(result.status == 'FILLED'){
+            isOperated = false
+            if(sell_amount > spot_btc_amount){
+                borrow_time = Date.now()
+                borrow_btc_amount += sell_amount - spot_btc_amount
+                spot_btc_amount = 0
+            }else{
+                spot_btc_amount -= sell_amount
+            }
+        }
+    })
+        .catch(error => client.logger.error(error))
+}
+
+// 普通模式买
+function buy_btc(buy_amount) {
+ 
+    client.newMarginOrder(
+        'BTCFDUSD', // symbol
+        'BUY',
+        'MARKET',
+        {
+          quantity: buy_amount,
+          newOrderRespType: 'RESULT',
+          sideEffectType: 'NO_SIDE_EFFECT',
+        }
+      ).then(response => {
+        const result = response.data
+        if(result.status == 'FILLED'){
+            isOperated = false
+            spot_btc_amount += buy_amount;
+        }
+    })
+        .catch(error => client.logger.error(error))
+}
+
+// 普通模式卖
+function sell_btc(sell_amount) {
+
+    client.newMarginOrder(
+        'BTCFDUSD', // symbol
+        'SELL',
+        'MARKET',
+        {
+          quantity: sell_amount,
+          newOrderRespType: 'RESULT',
+          sideEffectType: 'NO_SIDE_EFFECT',
+        }
+      ).then(response => {
+        const result = response.data
+        if(result.status == 'FILLED'){
+            isOperated = false
+            spot_btc_amount -= sell_amount;
+        }
+    })
+        .catch(error => client.logger.error(error))
+}
+
+function repay_buy_btc(buy_amount) {  // 自动还款模式
+
+    client.newMarginOrder(
+        'BTCFDUSD', // symbol
+        'BUY',
+        'MARKET',
+        {
+          quantity: buy_amount,
+          newOrderRespType: 'RESULT',
+          sideEffectType: 'AUTO_REPAY',
+        }
+      ).then(response => {
+        const result = response.data
+        if(result.status == 'FILLED'){
+            isOperated = false
+            spot_btc_amount += buy_amount - borrow_btc_amount;
+            borrow_time = 0;
+            borrow_btc_amount = 0;
+        }
+    })
+        .catch(error => client.logger.error(error))
+}
+
+function hand_repay_btc(repay_amount) {
+
+    client.marginRepay(
+        'BTC',
+        (repay_amount + 0.001)
+      ).then(response => {
+        isOperated = false
+        if(borrow_btc_amount > spot_btc_amount){
+            borrow_btc_amount -= spot_btc_amount
+            if(borrow_btc_amount == 0){
+                borrow_time = 0
+            }
+            spot_btc_amount = 0
+        }else{
+            spot_btc_amount -= borrow_btc_amount
+            borrow_btc_amount = 0
+            borrow_time = 0
+        }
+    })
+        .catch(error => client.logger.error(error))
+}
+
+
+// buy_btc()
+
+// sell_btc(0.00099)
+
+// hand_repay_btc()
